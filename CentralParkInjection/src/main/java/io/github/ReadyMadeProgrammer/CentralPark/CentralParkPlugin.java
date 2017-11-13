855b313808dcb3a0696ed8bcbb389e808485a6d3
ReadyMadeProgrammer <jun2620354@naver.com>
2017-11-13 21:04:23 +0900

    Change Injection Manager to Injection

--- CentralParkInjection/src/main/java/io/github/ReadyMadeProgrammer/CentralPark/CentralParkPlugin.java
+++ CentralParkInjection/src/main/java/io/github/ReadyMadeProgrammer/CentralPark/CentralParkPlugin.java
@@ -1,26 +1,23 @@
 package io.github.ReadyMadeProgrammer.CentralPark;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.glassfish.hk2.utilities.Binder;
 
 import java.util.Collections;
 import java.util.List;
 
 public abstract class CentralParkPlugin extends JavaPlugin {
-    private InjectionManager injectionManager;
+    private Injection injection;
     public final void onEnable(){
-        injectionManager = new InjectionManager(this);
-        injectionManager.onEnablePlugin();
+        injection = Injection.getInstance();
+        injection.load(this, this.getClassLoader());
         // TODO: 17. 11. 6 Write Install Checker
         installDDL();
     }
     public final void onDisable(){
-        injectionManager.onDisablePlugin();
+
     }
-    public List<Binder> binders(){
-        return Collections.emptyList();
-    }
-    public List<Class<?>> databaseClasses(){
+
+    private List<Class<?>> databaseClasses() {
         return Collections.emptyList();
     }
     public final List<Class<?>> getDatabaseClasses(){
